e3f8520d97249fc674bff479157d4f65a5890e6d
samuele865 <samuele.baldi2@stud.unifi.it>
2023-03-27 17:29:02 +0200

    Added support for channels and depth

---
+++ version.h
@@ -0,0 +1,12 @@
+//
+// Created by samuele on 27/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string version = "1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
